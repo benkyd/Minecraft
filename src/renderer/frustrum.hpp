dc8d6c2741a89dec32bdce53377b5bc16202d8af
Ben Kyd <benjaminkyd@gmail.com>
2019-11-07 19:49:39 +0000

    switching computers

--- src/renderer/frustrum.hpp
+++ src/renderer/frustrum.hpp
@@ -1,7 +1,35 @@
 #ifndef MINECRAFT_RENDERER_FRUSTRUM_H_
 #define MINECRAFT_RENDERER_FRUSTRUM_H_
 
+#include "../common.hpp"
+
+namespace EFrustrumPlanes {
+
+    enum Planes {
+
+        Right,
+        Left,
+        Top,
+        Bottom,
+        Far,
+        Near
+
+    };
+
+};
+
+class FrustrumPlane {
+public:
 
 
+
+};
+
+class Frustrum {
+public:
+
+
+
+};
 
 #endif
