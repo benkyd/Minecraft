1ce214bf7f160bd65a000064e0565e0e2f2949a4
Ben <benjaminkyd@gmail.com>
2020-05-15 14:59:51 +0100

    Started complete rewrite, i learned an awful lot but this is unsalvegable

--- legacy/src/common.hpp
+++ legacy/src/common.hpp
@@ -3,6 +3,7 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/hash.hpp>
 #include <glm/glm.hpp>
 
--- legacy/src/main.cpp
+++ legacy/src/main.cpp
@@ -2,7 +2,6 @@
 
 #include "game.hpp"
 
-
 int main(int argc, char** argv) {
 
 	Game game;
